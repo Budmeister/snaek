ecd6bd5f9b60314c7b8eb4aae6c848e0afe8847d
Brian Smith <brian.d.smith10@gmail.com>
2024-01-09 16:09:15 -0600

    Added level Lakes - put Rivers on hol

--- src/snaek/levels.rs
+++ src/snaek/levels.rs
@@ -1,6 +1,6 @@
 use rand::Rng;
 
-use super::{types::{GameState, Coord}, art::{PlusLava, BoardArt}};
+use super::{types::{GameState, Coord}, art::{PlusLava, BoardArt, PlusWater}};
 
 pub struct Level {
     pub name: &'static str,
@@ -11,22 +11,75 @@ pub struct Level {
 
 pub static LEVELS: &[Level] = &[
     Level {
-        name: "Rivers",
-        raw_board: RIVERS_LEVEL,
+        name: "Lakes",
+        raw_board: LAKES_LEVEL,
         index: 0,
-        new_level_state: || Box::new(RiversState),
+        new_level_state: || Box::new(LakesState::new()),
     }
 ];
 
+const WEATHER_DURATION_MIN: usize = 50;
+const WEATHER_DURATION_MAX: usize = 250;
 pub trait LevelState {
     fn update(&mut self, s: &mut GameState);
 }
 
-struct RiversState;
-impl LevelState for RiversState {
+struct LakesState {
+    weather: Weather,
+}
+struct Weather {
+    duration: usize,
+    kind: WeatherType
+}
+enum WeatherType {
+    None,
+    LavaRain,
+    WaterRain,
+}
+impl LevelState for LakesState {
     fn update(&mut self, s: &mut GameState) {
-        let coord: Coord = rand::thread_rng().gen();
-        s.board.pt(coord, PlusLava(1));
+        match self.weather.kind {
+            WeatherType::None => {}
+            WeatherType::LavaRain => {
+                let coord: Coord = rand::thread_rng().gen();
+                s.board.pt(coord, PlusLava(1));
+            }
+            WeatherType::WaterRain => {
+                let coord: Coord = rand::thread_rng().gen();
+                s.board.pt(coord, PlusWater(1));
+            }
+        }
+        if self.weather.duration > 0 {
+            self.weather.duration -= 1;
+        } else {
+            self.next_weather();
+        }
+    }
+}
+impl LakesState {
+    fn new() -> LakesState {
+        Self {
+            weather: Weather {
+                duration: Self::rand_weather_duration(),
+                kind: WeatherType::None,
+            }
+        }
+    }
+    fn next_weather(&mut self) {
+        let kind = match rand::thread_rng().gen_range(0..3) {
+            0 => WeatherType::None,
+            1 => WeatherType::LavaRain,
+            _ => WeatherType::WaterRain,
+        };
+        self.weather = Weather {
+            duration: Self::rand_weather_duration(),
+            kind,
+        }
+    }
+    fn rand_weather_duration() -> usize {
+        let duration = rand::thread_rng().gen_range(WEATHER_DURATION_MIN..WEATHER_DURATION_MAX);
+        println!("Random weather duration: {}", duration);
+        duration
     }
 }
 
@@ -37,6 +90,7 @@ pub static WATER_V_LAVA_LEVEL: &[u8] = include_bytes!("../../res/levels/water_v_
 pub static THREE_BASINS_LEVEL: &[u8] = include_bytes!("../../res/levels/three_basins.bin");
 pub static HUMBLE_BEGINNINGS_LEVEL: &[u8] = include_bytes!("../../res/levels/humble_beginnings.bin");
 pub static RIVERS_LEVEL: &[u8] = include_bytes!("../../res/levels/rivers.bin");
+pub static LAKES_LEVEL: &[u8] = include_bytes!("../../res/levels/lakes.bin");
 
 pub static SCORE_BANNER: &[u8] = include_bytes!("../../res/levels/score_banner.bin");
 pub static SCORE_BANNER_VERT: &[u8] = include_bytes!("../../res/levels/score_banner_vertical.bin");
--- todo.md
+++ todo.md
@@ -3,12 +3,14 @@
 2. ~~Water and Lava flow downhill~~
 3. ~~Water and Lava have a "height" value that, along with the elevation, determines where they flow~~
 4. ~~When water and lava meet, one "height" of each gets turned into one elevation~~
-5. Remove Seasons and add l: Box<dyn LevelState>, which shall be stored on the logic thread stack. Each level will provide its own implementation of LevelState, which will update the board every tick and provide shop information. Unlike GameState and ViewState, LevelState will be a fully fledged object with methods and can mutate itself, but the logic thread only cares that it implements LevelState.
-6. The current level, Rivers, will let the user buy seconds of rain, and the rest of the time, it will be raining lava. 
-7. Seed "dist" value has a max speed of 1 per tick
-8. Food no longer spawns naturally, but Gold does
-9. Powerups spawn less frequently
-10. Add shop where you can spend Gold for all powerups
-11. Add Shovel powerup: lets you pick up one piece of dirt and place it back down later
-12. Invincibility is an on-demand powerup
-13. Explosion also decreases elevation
+5. ~~Remove Seasons and add l: Box<dyn LevelState>, which shall be stored on the logic thread stack. Each level will provide its own implementation of LevelState, which will update the board every tick and provide shop information. Unlike GameState and ViewState, LevelState will be a fully fledged object with methods and can mutate itself, but the logic thread only cares that it implements LevelState.~~
+6. Fix lava colors
+7. Seed has a height value and dist value. If dist < 10, then height has 1% chance to increment, else decrements. Then, for each 5 height, has a 1% to spawn food (which can be red, light green, or dark green now) or coin. Empty can become seed by the same dist formula
+8. Seed height affects water flow. It can keep water from flowing, but if it does flow, it takes out the seed.
+9. If seed touches lava, it immediately becomes lava with the same height
+10. Seed "dist" value has a max speed of 1 per tick
+11. Food and powerups no longer spawn naturally. Player spawns with 5 waters, 3 seeds, and 10 coins
+12. Add shop where you can spend Gold for all powerups
+13. Add Shovel powerup: lets you pick up one piece of dirt and place it back down later
+14. Invincibility is an on-demand powerup
+15. Explosion also decreases elevation
